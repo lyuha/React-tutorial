d77df4d52e6449f9618b9b1298a227c06f79c8b7
Lyuha <lyuha@users.noreply.github.com>
2020-08-23 04:34:05 +0900

    Add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,8 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended"
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "prettier/@typescript-eslint"
   ]
 }
---
+++ .prettierrc.js
@@ -0,0 +1,4 @@
+module.exports = {
+  semi: true,
+  traillingComma: "all",
+};
