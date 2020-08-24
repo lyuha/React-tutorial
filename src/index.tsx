4195491212d29d83211350f60410a1adbf9e2f2d
Lyuha <lyuha@users.noreply.github.com>
2020-08-24 17:16:16 +0900

    Make highlight the three squares that caused win
    Finish "React Tutorial" include improvement tasks

--- src/index.tsx
+++ src/index.tsx
@@ -10,29 +10,43 @@ enum GameStatus {
 }
 
 function Square(props) {
-  return (
-    <button className="square" onClick={props.onClick} >
-      {props.value}
-    </button>
-  );
+  if (props.highlight) {
+    return (
+      <button className="square" onClick={props.onClick} style={{ backgroundColor: "#FF0000" }}>
+        {props.value}
+      </button>
+    );
+  } else {
+    return (
+      <button className="square" onClick={props.onClick} >
+        {props.value}
+      </button>
+    );
+  }
 }
 
 class Board extends React.Component {
-  renderSquare(i) {
+  renderSquare(i, highlight: boolean) {
     return (
       <Square
         key={i}
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)}
+        highlight={highlight}
       />
     );
   }
 
   render() {
     const boardSize = 3
+    const line = this.props.line;
 
     const board = [...Array(3)].map((_, i) => {
-      const row = [...Array(boardSize)].map((_, j) => this.renderSquare(i * boardSize + j));
+      const row = [...Array(boardSize)].map((_, j) => {
+        let index = i * boardSize + j;
+
+        return this.renderSquare(index, line.includes(index));
+      });
 
       return (
         <div className="board-row" key={i}>
@@ -120,7 +134,7 @@ class Game extends React.Component {
 
     const moves = this.state.isAscendingOrder ? unorderedMove : unorderedMove.reverse();
 
-    const gameStatus = calculateGameStatus(current.squares);
+    const [gameStatus, highlightedLine] = calculateGameStatus(current.squares);
 
     let status: string;
     switch (gameStatus) {
@@ -150,6 +164,7 @@ class Game extends React.Component {
         <div className="game-board">
           <Board
             squares={current.squares}
+            line={highlightedLine}
             onClick={(i) => this.handleClick(i)}
           />
         </div>
@@ -172,7 +187,7 @@ ReactDOM.render(
 
 // ========================================
 
-function calculateGameStatus(squares): GameStatus {
+function calculateGameStatus(squares): [GameStatus, [number]] {
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
@@ -187,15 +202,15 @@ function calculateGameStatus(squares): GameStatus {
   for (const line of lines) {
     const [a, b, c] = line;
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return [squares[a], line];
     }
   }
 
   for (const square of squares) {
     if (square === null) {
-      return GameStatus.Running;
+      return [GameStatus.Running, []];
     }
   }
 
-  return GameStatus.Draw;
+  return [GameStatus.Draw, []];
 }
